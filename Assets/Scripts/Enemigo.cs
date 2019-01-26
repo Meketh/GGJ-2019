e74319b4d5a558ab3acc9bc53070128b82fdf522
maxmouse <bolia.maximiliano@gmail.com>
2019-01-26 14:30:47 -0300

    Bala enemigo

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    float VidaEnemigo = 100;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bala")
+        {
+
+            VidaEnemigo -= 10;
+        }
+    }
+}
