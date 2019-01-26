76be04be27a39580d09e8ae3dd3f79173f384d6d
maxmouse <bolia.maximiliano@gmail.com>
2019-01-26 12:27:17 -0300

    Se gira la nariz del player y se fixea el script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 
 
     public float velocidadRotacion = 100;
-    public float velocidadTraslacion = 100;
+    public float velocidadTraslacion = 10;
 	// Use this for initialization
 	void Start () {
 		
