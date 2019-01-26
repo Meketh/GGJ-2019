be7616ee1b0ab262121ecd7aa5d99bfcd2006422
Lorenzocs <caballerolorenzo66@gmail.com>
2019-01-26 19:18:03 -0300

    Se sube el prefab del player de ale bien op

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,31 +8,44 @@ public class Player : MonoBehaviour
 
     public float velocidadRotacion = 100;
     public float velocidadTraslacion = 10;
+    public Animator anim;
 
     // Use this for initialization
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        anim.SetBool("Walk", false);
+        anim.SetBool("Idle", true);
+
+
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, velocidadRotacion * Time.deltaTime, 0);
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, -velocidadRotacion * Time.deltaTime, 0);
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
         }
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, 0, velocidadTraslacion * Time.deltaTime);
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, 0, -velocidadTraslacion * Time.deltaTime);
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
         }
 
     }
