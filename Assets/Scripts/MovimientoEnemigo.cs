3982c0aaef71bc9d3a96d1dafbcd7fb7daf5c633
maxmouse <bolia.maximiliano@gmail.com>
2019-01-26 11:35:00 -0300

    Mov Enemy y nucleo

---
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovimientoEnemigo : MonoBehaviour
+{
+    public float velocidad = 4f;
+    public bool siguiendo = true;
+    public float rangoDeteccion = 10f;
+    public float rangoAtaque = 1.5f;
+    public AudioClip sirena;
+    Nucleo nucleo;
+    NavMeshAgent agent;
+    public float cooldown = 1.5f;
+    public float cooldownTimer;
+
+
+    public Estados estadoActual;
+
+
+
+    public enum Estados
+    {
+        Estado1,
+        Estado2,
+
+    }
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        nucleo = FindObjectOfType<Nucleo>();
+        agent.speed = velocidad;
+        //SonidoSierna();
+    }
+
+    void FixedUpdate()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopAllCoroutines();
+        }
+
+        float distanciaAlnucleo = Vector3.Distance(transform.position, nucleo.transform.position);
+
+
+
+        if (cooldownTimer == 0)
+        {
+
+
+            if (siguiendo == true)//Encontro al nucleo
+            {
+                if (distanciaAlnucleo > rangoAtaque)//Si el enemigo esta lejos del pj entonces solo lo sigo
+                {
+
+
+                    agent.destination = nucleo.transform.position;//NavMeshAgent.Warp(nucleo.transform.position);//
+                    transform.LookAt(nucleo.transform.position);
+                }
+            }
+        }
+        else
+        {
+            siguiendo = false;
+        }
+    }
+
+    public void CooldownPararse()
+    {
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+        }
+        if (cooldownTimer < 0)
+        {
+            cooldownTimer = 0;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, rangoDeteccion);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, rangoAtaque);
+    }
+
+
+
+    
+}
