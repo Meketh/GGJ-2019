02332e4b8307a7d1c72b350d65dac3716bec7784
Lorenzocs <caballerolorenzo66@gmail.com>
2019-01-26 13:20:45 -0300

    Cubos Tags y Scripts

---
+++ Assets/Scripts/DesactivadorBloques.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivadorBloques : MonoBehaviour
+{
+
+    
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bloques" && Input.GetKey(KeyCode.Mouse1))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+
+}
