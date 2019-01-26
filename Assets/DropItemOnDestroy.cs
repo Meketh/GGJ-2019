292cde91f4d1960f2832e6646ec9dcbd2978488b
tbernabo <tomasbernabo@gmail.com>
2019-01-26 11:15:57 -0300

    push de mapa y scripts primeros

---
+++ Assets/DropItemOnDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemOnDestroy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] _items;
+
+    private void OnDestroy()
+    {
+        var itemIndex = Random.Range(0, _items.Length);
+
+        var newItem = Instantiate(_items[itemIndex]);
+    }
+}
---
+++ Assets/Scripts/Block_Destroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block_Destroy : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Destroy(col.gameObject);
+    }
+}
