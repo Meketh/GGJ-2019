3ea0fe77dcf97735003a40fedb40507b5e71dda5
Nahuel Santoalla <nahjersan@gmail.com>
2019-01-27 00:51:13 -0300

    disable cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
   public GameObject Bala;
-  public float speed = 30f;
+  float speed = 25f;
   float cooldown = 1.5f;
   float cooldownTimer = 0f;
   Vector3 cameraOffset = 30 * new Vector3(0, 2, -1);
+  float cameraSpeed = 20f;
 
   Animator anim;
   void Start() {
@@ -29,17 +30,23 @@ public class Player : MonoBehaviour {
       translation += displacement * Vector3.right;
     }
     transform.position += translation;
-    anim.SetBool("Walk", translation != Vector3.zero);
-    anim.SetBool("Idle", translation == Vector3.zero);
     if (cooldownTimer > 0) {
       cooldownTimer -= Time.deltaTime;
       if (Input.GetMouseButtonUp(0)) {
         anim.SetBool("Attack", false);
       }
-    } else if (cooldownTimer <= 0f && Input.GetKey(KeyCode.Space)) {
-      cooldownTimer = 0.5f;
-      anim.SetBool("Attack", true);
-      Instantiate(Bala, transform);
+    } else {
+      if (Input.GetKey(KeyCode.Space)) {
+        cooldownTimer = 0.5f;
+        anim.SetBool("Attack", true);
+        anim.SetBool("Walk", false);
+        anim.SetBool("Idle", false);
+        Instantiate(Bala, transform);
+      } else {
+        anim.SetBool("Attack", false);
+        anim.SetBool("Walk", translation != Vector3.zero);
+        anim.SetBool("Idle", translation == Vector3.zero);
+      }
     }
     float distance;
     var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -47,7 +54,11 @@ public class Player : MonoBehaviour {
     if (plane.Raycast(ray, out distance)) {
       transform.LookAt(ray.GetPoint(distance));
     }
-    Camera.main.transform.position = transform.position + cameraOffset;
+    Camera.main.transform.position = Vector3.MoveTowards(
+      Camera.main.transform.position,
+      transform.position + cameraOffset,
+      cameraSpeed * Time.deltaTime
+    );
     Camera.main.transform.LookAt(transform.position);
   }
 }
