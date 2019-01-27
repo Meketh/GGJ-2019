d2d5d517533e7c87b05e7c723b7a8bceb8df31f8
Nahuel Santoalla <nahjersan@gmail.com>
2019-01-27 02:50:13 -0300

    add bullet types

--- Assets/Scripts/MovimientoBala.cs
+++ Assets/Scripts/MovimientoBala.cs
@@ -6,16 +6,28 @@ public class MovimientoBala : MonoBehaviour {
   public int velocidad = 150;
   public int danio = 50;
   public float ttl = 3;
+  public int fragments = 0;
+  float fragmentArc = 20f;
   void Update() {
     gameObject.transform.Translate(0f, 0f, velocidad * Time.deltaTime);
     Invoke("destroyGameObject", ttl);
   }
-  private void destroyGameObject() {
-    Destroy(gameObject);
-  }
-  private void OnTriggerEnter(Collider other) {
-    if(other.gameObject.tag != "Player") {
+  void OnTriggerEnter(Collider other) {
+    if (other.gameObject.tag != "Player"
+    && other.gameObject.tag != "Bala") {
       destroyGameObject();
     }
+  }
+  void destroyGameObject() {
+    Destroy(gameObject);
+    if (fragments > 0) {
+      newBullet(-fragmentArc);
+      newBullet(fragmentArc);
+    }
+  }
+  void newBullet(float arc = 0) {
+    var bullet = Instantiate(gameObject, transform.position, transform.rotation);
+    bullet.GetComponent<MovimientoBala>().fragments = fragments - 1;
+    bullet.transform.Rotate(0, arc, 0);
   }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum BulletType { Normal, Scatter, Rapid, Fragment }
+
 public class Player : MonoBehaviour {
   public GameObject Bala;
   AudioSource sourceAudio;
   SoundManager soundScrpt;
   float speed = 25f;
-  float cooldown = 1.5f;
-  float cooldownTimer = 0f;
+  float cooldown = 0f;
   Vector3 cameraOffset = 30 * new Vector3(0f, 3f, -1.5f);
   float cameraSpeed = 20f;
+  BulletType bulletType = BulletType.Normal;
+  float fragmentArc = 20f;
 
   Animator anim;
   void Start() {
     anim = GetComponent<Animator>();
     sourceAudio = FindObjectOfType<AudioSource>().GetComponent<AudioSource>();
     soundScrpt = FindObjectOfType<SoundManager>().GetComponent<SoundManager>();
-    }
+  }
   void Update() {
     var translation = new Vector3();
     var displacement = speed * Time.deltaTime;
+    if (Input.GetKey(KeyCode.N)) {
+      bulletType = BulletType.Normal;
+    }
+    if (Input.GetKey(KeyCode.R)) {
+      bulletType = BulletType.Rapid;
+    }
+    if (Input.GetKey(KeyCode.F)) {
+      bulletType = BulletType.Fragment;
+    }
+    if (Input.GetKey(KeyCode.V)) {
+      bulletType = BulletType.Scatter;
+    }
     if (Input.GetKey(KeyCode.W)) {
       translation += displacement * Vector3.forward;
     }
@@ -34,16 +49,26 @@ public class Player : MonoBehaviour {
       translation += displacement * Vector3.right;
     }
     transform.position += translation;
-    if (cooldownTimer > 0) {
-      cooldownTimer -= Time.deltaTime;
+    if (cooldown > 0) {
+      cooldown -= Time.deltaTime;
     } else {
       if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) {
-        cooldownTimer = 0.5f;
+        var bullet = newBullet();
+        cooldown = 1f;
+        switch (bulletType) {
+          case BulletType.Rapid: cooldown = .5f; break;
+          case BulletType.Fragment:
+          bullet.GetComponent<MovimientoBala>().fragments = 3;
+          break;
+          case BulletType.Scatter:
+          newBullet(-fragmentArc);
+          newBullet(fragmentArc);
+          break;
+        }
+        sourceAudio.PlayOneShot(soundScrpt.clipMisil, 0.5f);
         anim.SetBool("Attack", true);
         anim.SetBool("Walk", false);
         anim.SetBool("Idle", false);
-        Instantiate(Bala, transform.position, transform.rotation);
-        sourceAudio.PlayOneShot(soundScrpt.clipMisil, 0.5f);
       } else {
         anim.SetBool("Attack", false);
         anim.SetBool("Walk", translation != Vector3.zero);
@@ -62,5 +87,10 @@ public class Player : MonoBehaviour {
       cameraSpeed * Time.deltaTime
     );
     Camera.main.transform.LookAt(transform.position);
+  }
+  GameObject newBullet(float arc = 0) {
+    var bullet = Instantiate(Bala, transform.position, transform.rotation);
+    bullet.transform.Rotate(0, arc, 0);
+    return bullet;
   }
 }
