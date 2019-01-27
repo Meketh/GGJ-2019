9e27f1ded1ed23965b5b8f9229793d016cceff43
Lorenzocs <caballerolorenzo66@gmail.com>
2019-01-26 23:36:52 -0300

    GameOver UI
    GameOver U

---
+++ Assets/PrenderYApagarGameOver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrenderYApagarGameOver : MonoBehaviour
+{
+    public Text textoGameOver;
+    public Text textoLore;
+    public Text textoRetry;
+    
+    private void Start()
+    {
+        textoGameOver.enabled = false;
+        textoLore.enabled = false;
+        textoRetry.enabled = false;
+
+        FuncionLore();
+        Invoke("FuncionGameOveryRetry", 7f);
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    void FuncionLore()
+    {
+        textoLore.enabled = true;      
+    }
+
+    void FuncionGameOveryRetry()
+    {
+        textoLore.enabled = false;
+        textoGameOver.enabled = true;
+        textoRetry.enabled = true;
+    }
+
+}
