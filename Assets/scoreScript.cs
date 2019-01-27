1b8ede878a89b08b2cd029354a2f473dd0dfa6d5
maxmouse <bolia.maximiliano@gmail.com>
2019-01-27 12:33:58 -0300

    SCORE FUNCIONANDO

---
+++ Assets/scoreScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreScript : MonoBehaviour
+{
+    int scoreText;
+    public Text textoScore;
+    LevelManager levelScript;
+
+    private void Start()
+    {
+        levelScript = FindObjectOfType<LevelManager>().GetComponent<LevelManager>();
+        scoreText = 0;
+    }
+
+    private void Update()
+    {
+        textoScore.text = levelScript.contadorEnemigos.ToString()+ " / 100";
+    }
+
+
+}
