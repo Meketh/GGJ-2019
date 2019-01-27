5ebcb31bcc00c5caceac925efdb448eef1a03c9b
Lorenzocs <caballerolorenzo66@gmail.com>
2019-01-26 21:46:26 -0300

    Sonidos, canvas vida

---
+++ Assets/BarraDeVidaNucleo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraDeVidaNucleo : MonoBehaviour
+{
+    Nucleo nucleoScript;
+    Image imagenVerde;
+
+    float vidaActual;
+    float maxHealth;
+
+    void Start()
+    {
+        nucleoScript = FindObjectOfType<Nucleo>().GetComponent<Nucleo>();
+        imagenVerde = GetComponent<Image>();
+        maxHealth = nucleoScript.vidaNucleo;
+    }
+
+    void Update()
+    {
+        vidaActual = nucleoScript.vidaNucleo;
+        imagenVerde.fillAmount = vidaActual / maxHealth;
+
+    }
+
+}
