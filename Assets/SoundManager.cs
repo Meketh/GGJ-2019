03d03ddc64d85b714d8000fc83cfcc30bbaa45da
maxmouse <bolia.maximiliano@gmail.com>
2019-01-26 21:54:28 -0300

    script soundManager

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static SoundManager Instance;
+    private AudioSource audioSource;
+    public AudioClip clipBackground;
+
+    void Awake()//Inicializa scripts/variables que lo van a utilizar por ejemplo Start. Primero pasa por todos los Awake.
     {
-        
+        if (Instance == null)
+        {
+            Instance = this; //this, hace referencia a este script.
+            DontDestroyOnLoad(gameObject);// No te destruyas, a este objeto.El objeto seria el que contiene el script.
+
+        }
+        else
+        {
+            Destroy(gameObject);
+
+        }
+
+        audioSource = GetComponent<AudioSource>();
+        playBackground(clipBackground);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void playBackground(AudioClip clip)
     {
-        
+        audioSource.PlayOneShot(clip, 0.1f);
+
+    }
+
+    public void playAudio(AudioClip clip, float vol = 1f)
+    {
+        audioSource.PlayOneShot(clip, vol);
+
     }
 }
