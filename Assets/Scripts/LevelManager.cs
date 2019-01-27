c5bc1bd8f28b52f28f6e1f8f927d728df5649e79
Nahuel Santoalla <nahjersan@gmail.com>
2019-01-27 04:13:45 -0300

    add spawn acceleration

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
-  float frecuenciaAparicionMinion = 3f;
-  float delayParaEmpezar = 1f;
+  float spawnTime = 6f;
   public GameObject enemigo;
   public GameObject[] poolSpawners;
   public float timeInvoke;
   public float repeatInvoke;
   public int contadorEnemigos = 0;
   void Start() {
-    InvokeRepeating("InvocarMinion", delayParaEmpezar, frecuenciaAparicionMinion);
+    Invoke("InvocarMinion", spawnTime);
+    InvokeRepeating("speedSpawning", 0f, 10f);
+  }
+  public void speedSpawning() {
+    spawnTime = Mathf.Max(.5f, spawnTime - 1f);
   }
   public void InvocarMinion() {
+    Invoke("InvocarMinion", spawnTime);
     Instantiate(enemigo, //transform.position, transform.rotation);
       poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.position,
       poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.rotation);
