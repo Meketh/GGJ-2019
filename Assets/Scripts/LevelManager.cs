db78151e8082d11e367853a21a7ad4d41686fd92
Nahuel Santoalla <nahjersan@gmail.com>
2019-01-27 03:07:37 -0300

    remove print

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,40 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
-{
-    float frecuenciaAparicionMinion = 3f;
-    float delayParaEmpezar = 1f;
-    public GameObject enemigo;
-    public GameObject[] poolSpawners;
-    public float timeInvoke;
-    public float repeatInvoke;
-    public int contadorEnemigos = 0;
-
-    void Start()
-    {
-        InvokeRepeating("InvocarMinion", delayParaEmpezar, frecuenciaAparicionMinion);
+public class LevelManager : MonoBehaviour {
+  float frecuenciaAparicionMinion = 3f;
+  float delayParaEmpezar = 1f;
+  public GameObject enemigo;
+  public GameObject[] poolSpawners;
+  public float timeInvoke;
+  public float repeatInvoke;
+  public int contadorEnemigos = 0;
+  void Start() {
+    InvokeRepeating("InvocarMinion", delayParaEmpezar, frecuenciaAparicionMinion);
+  }
+  public void InvocarMinion() {
+    Instantiate(enemigo, //transform.position, transform.rotation);
+      poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.position,
+      poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.rotation);
+  }
+  void Invocacion() {
+    if (contadorEnemigos <= 10) {
+      contadorEnemigos += 1;
+      //Instantiate(enemigos, transform.position, transform.rotation);
     }
-
-    public void InvocarMinion()
-    {
-        print("invocar minion");
-        Instantiate(enemigo, //transform.position, transform.rotation);
-            poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.position,
-            poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.rotation);
-
-    }
-
-
-    void Invocacion()
-    {
-
-        if (contadorEnemigos <= 10)
-        {
-            contadorEnemigos += 1;
-            //Instantiate(enemigos, transform.position, transform.rotation);
-        }
-
-
-    }
+  }
 }
