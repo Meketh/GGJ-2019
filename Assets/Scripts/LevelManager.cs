e39b12ac0fa10c8d5d2197e7ca5a69d86b47ef09
maxmouse <bolia.maximiliano@gmail.com>
2019-01-27 01:17:47 -0300

    Se agrega el levelManager para que se spawneen de distantas direcciones con un random

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    float frecuenciaAparicionAutos = 3f;
+    float frecuenciaAparicionMinion = 3f;
     float delayParaEmpezar = 1f;
     public GameObject enemigo;
     public GameObject[] poolSpawners;
+    public float timeInvoke;
+    public float repeatInvoke;
+    public int contadorEnemigos = 0;
 
     void Start()
     {
-        InvokeRepeating("InvocarAuto", delayParaEmpezar, frecuenciaAparicionAutos);
+        InvokeRepeating("InvocarMinion", delayParaEmpezar, frecuenciaAparicionMinion);
     }
 
-    public void InvocarAuto()
+    public void InvocarMinion()
     {
-        GameObject.Instantiate(enemigo, //transform.position, transform.rotation);
+        print("invocar minion");
+        Instantiate(enemigo, //transform.position, transform.rotation);
             poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.position,
             poolSpawners[Random.Range(0, poolSpawners.Length - 1)].transform.rotation);
+
+    }
+
+
+    void Invocacion()
+    {
+
+        if (contadorEnemigos <= 10)
+        {
+            contadorEnemigos += 1;
+            //Instantiate(enemigos, transform.position, transform.rotation);
+        }
+
 
     }
 }
